ac221c914fc20f6fafdaf526a46c2314819841a9
Araf Rahman <arahman.usa500@gmail.com>
2020-11-06 10:22:11 -0500

    Fluff

--- src/pages/modules/Modules.js
+++ src/pages/modules/Modules.js
@@ -8,8 +8,8 @@ export default function Modules() {
     return (
         <>
             <Cards />
-            <Footer />
             <ChartSample />
+            <Footer />
         </>
     )
 }
