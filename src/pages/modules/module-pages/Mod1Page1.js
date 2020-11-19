f9a0f5fdbdb56f7261f165c9e52a9836a81733b9
Araf Rahman <arahman.usa500@gmail.com>
2020-11-18 21:09:30 -0500

    fixed images

--- src/pages/modules/module-pages/Mod1Page1.js
+++ src/pages/modules/module-pages/Mod1Page1.js
@@ -3,6 +3,8 @@ import '../../../App.css'
 import ModuleNavFooter from '../module-components/ModuleNavFooter'
 import ModuleNavHeader from '../module-components/ModuleNavHeader'
 import './Module.css'
+import checkpoint from "../../../images/checkpoints.jpg"
+
 
 export default function Mod1Page1() {
     return (
@@ -10,7 +12,7 @@ export default function Mod1Page1() {
         <div>
             <div> 
                 <ModuleNavHeader title='Page 2' prevLink='/module-1' nextLink='/module-2'/>
-                <p><img src="images/checkpoints.jpg" alt="checkpoints" width="200" height="300" className="embed-image"/></p>
+                <p><img src={checkpoint}  alt="checkpoints" width="200" height="300" className="embed-image"/></p>
                 <p className='content'>There is definitely some way that is fastest to figure out what to do here, but it might take some time for a person to figure it out. For Prim’s algorithm, though, it can figure it out in seconds.
                 <br/>
                 <br/>The way it works is it looks at each checkpoint we have visited, and then looks at all possible edges it can go to from any of the checkpoints we have already visited. Then, it will pick the edge that is the smallest.                
--- src/pages/modules/module-pages/Mod1Page2.js
+++ src/pages/modules/module-pages/Mod1Page2.js
@@ -3,6 +3,7 @@ import '../../../App.css'
 import ModuleNavFooter from '../module-components/ModuleNavFooter'
 import ModuleNavHeader from '../module-components/ModuleNavHeader'
 import './Module.css'
+import PrimAlgDemo from "../../../images/PrimAlgDemo.gif"
 
 export default function Mod1Page2() {
     return (
@@ -11,7 +12,7 @@ export default function Mod1Page2() {
             <div> 
                 <ModuleNavHeader title='Page 3' prevLink='/module-1' nextLink='/module-2'/>
                 <p className='content'>
-                <img src="images/PrimAlgDemo.gif" className="embed-image" alt="gif" width="200" height="300" />
+                <img src={PrimAlgDemo} className="embed-image" alt="gif" width="200" height="300" />
                 <br />Note that you can traverse any edge to any node no matter which one was last visited by the algorithm. You are not restricted to the immediate neighbors of a node, you are restricted to all neighbors of all nodes visited so far.
                 <br/>
                 <br />Prim's is useful when you are not given information ahead of time, and is useful when you need to visit every single possible node, rather than just getting from one node to another. Keep in mind when we make our immediate first decision between taking the road that is 2 miles, or taking the road that is 1 mile to reach a checkpoint, we are completely unaware of how many checkpoints there are, and the distances between them.
